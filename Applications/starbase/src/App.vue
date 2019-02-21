e952d45daeeb0d8124747dfc147e6d9acb3fb185
Ordana <Ordana1234@gmail.com>
2019-02-21 15:57:11 +0100

    New App ! it's Starbase pew pew pew

---
+++ Applications/starbase/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="app">
+        <h1> {{title}} </h1> 
+        <div class="row">  
+            <div class="col-md-4" v-for="(id, index) in ids" :key="index">
+                <Character :id="id" />
+            </div>
+        </div>     
+    </div>
+</template>
+
+<script>
+import Character from './components/Character.vue'
+
+export default {
+    name    :   'app',
+    data() {
+        return {
+            title   :   'Generate Your Team',
+            ids : [1,13,14]
+        }
+    },
+    components : {
+        Character
+    }
+
+}
+</script>
