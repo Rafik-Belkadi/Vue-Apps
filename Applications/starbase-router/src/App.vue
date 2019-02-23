2c99aa9781d36dcd8c985e0010e07d2e939e9a97
Ordana <Ordana1234@gmail.com>
2019-02-23 14:49:46 +0100

    New App ! Its the Starbase app but with a Router this time, we are using the vue-router

---
+++ Applications/starbase-router/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <h1> {{title}} </h1>
+        <h4>
+            <router-link class="link" to="/data/people">Meet The Characters</router-link> |
+            <router-link class="link" to="/data/planets">Explore The Planets</router-link>
+        </h4>
+        <router-view></router-view>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    data() {
+        return {
+            title : 'Starbase-router'
+        }
+    }
+}
+</script>
